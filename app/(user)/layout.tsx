d34e1fe72c559a6180bcd82a678a48564b5b52b8
Aviral Jain <aviraljain1311@gmail.com>
2023-07-26 19:30:23 +0530

    update vercel analytics

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,7 +1,7 @@
 import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { AnalyticsWrapper } from '../components/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,7 +35,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <AnalyticsWrapper />
+        <Analytics />
       </body>
     </html>
   )
