78971f57031f703aab28c445618a4befa788e65e
Aviral Jain <aviraljain1311@gmail.com>
2023-09-22 11:15:39 +0530

    update opengraph img

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -14,13 +14,6 @@ export const metadata: Metadata = {
     description: 'Welcome to Aviral\'s Portfolio! I am a skilled and passionate full-stack developer with expertise in creating robust and user-friendly web applications. Explore my diverse range of projects, demonstrating proficiency in various front-end and back-end technologies. Connect with me to discuss exciting opportunities or to start a new project together.',
     url: 'https://aviral.xyz',
     siteName: "Aviral's Portfolio",
-    images: [
-      {
-        url: "https://aviral.xyz/og-image.jpg",
-        width: 1920,
-        height: 1080,
-      },
-    ],
     locale: "en-US",
     type: "website"
   }
