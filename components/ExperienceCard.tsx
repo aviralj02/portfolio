b686de9686417716212f61a8aff0ba95cb4e4bb7
Aviral Jain <aviraljain1311@gmail.com>
2024-01-02 01:42:55 +0530

    feat: :lipstick: update experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,50 +1,61 @@
-import urlFor from '@/lib/urlFor';
-import { motion } from 'framer-motion';
-import React from 'react';
+import urlFor from "@/lib/urlFor";
+import { motion } from "framer-motion";
+import React from "react";
 
 type Props = {
-  experience: Experience
-}
+  experience: Experience;
+};
 
 const ExperienceCard = ({ experience }: Props) => {
   return (
-    <div className='flex flex-col rounded-lg items-center space-y-7 bg-[#292929] p-10 flex-shrink-0 w-[400px] md:w-[800px] snap-center md:opacity-70 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-      <motion.img
-        initial={{ y: -100, opacity: 0 }}
-        whileInView={{ y: 0,opacity: 1 }}
-        transition={{ duration: 1.2 }}
-        viewport={{ once: true }}
-        className='w-20 h-20 md:w-24 md:h-24 rounded-full object-cover object-center'
-        src={urlFor(experience?.companyImage).url()}
-        alt="companyLogo"
-      />
+    <div className="flex flex-col rounded-lg space-y-7 bg-[#292929] p-10 flex-shrink-0 w-[400px] md:w-[600px] snap-center md:opacity-70 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <div className="flex gap-3 items-center">
+        <motion.img
+          initial={{ y: -100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.2 }}
+          viewport={{ once: true }}
+          className="w-16 h-16 md:w-20 md:h-20 rounded-full object-cover object-center"
+          src={urlFor(experience?.companyImage).url()}
+          alt="companyLogo"
+        />
+        <div className="flex flex-col">
+          <h4 className="text-xl md:text-2xl font-semibold">
+            {experience.company}
+          </h4>
+          <p className="font-normal text-lg md:text-xl mt-1">
+            {experience.jobTitle}
+          </p>
+        </div>
+      </div>
 
-      <div className='px-8'>
-        <h4 className='text-2xl font-light'>{experience.jobTitle}</h4>
-        <p className='font-bold text-xl mt-1'>{experience.company}</p>
-        <div className='flex space-x-2 my-2'>
+      <div className="px-4">
+        <div className="flex space-x-2">
           {/* Tech Used */}
           {experience.technologies.map((technology) => (
             <img
               key={technology._id}
-              className='h-8 w-8 rounded-full object-cover object-center' 
-              src={urlFor(technology.image).url()} 
-              alt={technology.title} />
+              className="h-8 w-8 rounded-full object-cover object-center"
+              src={urlFor(technology.image).url()}
+              alt={technology.title}
+            />
           ))}
         </div>
-        <p className='uppercase py-5 text-gray-300 text-sm'>
-          {new Date(experience.dateStarted).toDateString()} - {" "}
-          {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
+        <p className="uppercase py-5 text-gray-300 text-xs md:text-sm">
+          {new Date(experience.dateStarted).toDateString()} -{" "}
+          {experience.isCurrentlyWorkingHere
+            ? "Present"
+            : new Date(experience.dateEnded).toDateString()}
         </p>
 
-        <ul className='list-disc space-y-4 ml-5 text-xs md:text-sm'>
+        <ul className="list-disc space-y-2 ml-4 text-xs md:text-sm">
           {experience.points.map((point, index) => (
             <li key={index}>{point}</li>
           ))}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ExperienceCard;
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { motion } from 'framer-motion';
-import ExperienceCard from './ExperienceCard';
+import React from "react";
+import { motion } from "framer-motion";
+import ExperienceCard from "./ExperienceCard";
 
 type Props = {
-  experiences: Experience[]
-}
+  experiences: Experience[];
+};
 
 const WorkExperience = ({ experiences }: Props) => {
   return (
@@ -14,22 +14,19 @@ const WorkExperience = ({ experiences }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='h-screen flex flex-col relative text-left max-w-full px-10 justify-center items-center mx-auto md:gap-5'
+      className="h-screen flex flex-col relative text-left max-w-full sm:px-10 justify-center items-center mx-auto md:gap-5"
     >
-      <h3 className='mt-14 sm:mt-10 uppercase tracking-[15px] text-gray-500 text-2xl -mr-[15px]'>
+      <h3 className="uppercase tracking-[15px] text-gray-500 text-2xl -mr-[15px]">
         Experience
       </h3>
 
-      <div className='max-w-6xl w-full flex space-x-5 overflow-x-scroll p-5 snap-x snap-mandatory scrollbarStyle'>
+      <div className="max-w-6xl w-full flex space-x-5 overflow-x-scroll p-5 snap-x snap-mandatory scrollbarStyle">
         {experiences.map((experience: Experience) => (
-          <ExperienceCard 
-            key={experience._id}
-            experience={experience}
-          />
+          <ExperienceCard key={experience._id} experience={experience} />
         ))}
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
 export default WorkExperience;
